04d2b7a339104cee350775d9af2e650e37f44031
[dyad] <jasfconsultoria@gmail.com>
2025-08-19 18:11:25 -0300

    [dyad] Conectado ao novo projeto Supabase - wrote 1 file(s) + extra files edited outside of Dyad

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ntmwazhdwegdhiiitxli.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bXdhemhkd2VnZGhpaWl0eGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MzExMTQsImV4cCI6MjA3MTIwNzExNH0.CUQqnRtRW2xivigIN3riMo7fylwxzNg9_6kMTRoOYy8';
+const supabaseUrl = 'https://yyjarlodmftfngnevwjd.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5amFybG9kbWZ0Zm5nbmV2d2pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MzY4MjAsImV4cCI6MjA3MTIxMjgyMH0.jgg8nzfXk5nMjRoqNPbyRx8Qe-okWxq1e3QMiS2Tapg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
